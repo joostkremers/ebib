33a8774048f5dfbb8bb7b6e82adfc88871491305
Joost Kremers <joostkremers@users.sourceforge.net>
2010-09-12 03:00:58 +0200

    Wrap (require 'cl) in eval-when-compile

--- src/ebib.el
+++ src/ebib.el
@@ -26,7 +26,8 @@
 ;; (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
 ;; THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-(require 'cl)
+(eval-when-compile
+  (require 'cl))
 (require 'easymenu)
 
 ;;;;;;;;;;;;;;;;;;;;;;
