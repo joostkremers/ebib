11f12857388dccf2961f5dcfb10ad30a9046dbf5
Joost Kremers <joostkremers@yahoo.com>
2012-03-11 18:20:12 +0100

    Remove reference to XEmacs from the manual and README.md
    eventually i'll get around to testing Ebib with XEmacs again. or not...

--- README.md
+++ README.md
@@ -2,10 +2,10 @@ Ebib
 
 (c) 2003-2012 Joost Kremers
 
-Ebib is a BibTeX database manager that runs in GNU Emacs and
-XEmacs. With Ebib, you can create and manage .bib-files, all within
-Emacs. It supports @string and @preamble definitions, multi-line field
-values, searching, and integration with Emacs' (La)TeX mode.
+Ebib is a BibTeX database manager that runs in GNU Emacs. With Ebib, you
+can create and manage .bib-files, all within Emacs. It supports @string and
+@preamble definitions, multi-line field values, searching, and integration
+with Emacs' (La)TeX mode.
 
 See the Ebib manual for usage and installation instructions.
 
--- src/ebib.el
+++ src/ebib.el
@@ -34,10 +34,10 @@
 
 ;;; Commentary:
 
-;; Ebib is a BibTeX database manager that runs in GNU Emacs and
-;; XEmacs. With Ebib, you can create and manage .bib-files, all within
-;; Emacs. It supports @string and @preamble definitions, multi-line field
-;; values, searching, and integration with Emacs' (La)TeX mode.
+;; Ebib is a BibTeX database manager that runs in GNU Emacs. With Ebib, you
+;; can create and manage .bib-files, all within Emacs. It supports @string
+;; and @preamble definitions, multi-line field values, searching, and
+;; integration with Emacs' (La)TeX mode.
 
 ;; See the Ebib manual for usage and installation instructions.
 
