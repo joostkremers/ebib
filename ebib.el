36d9d19abacaeade54a043701c5b2e4e58b8a61b
Joost Kremers <joostkremers@fastmail.fm>
2017-02-22 23:59:34 +0100

    Add extension to bib file name when using \jobname.
    Thanks to Elias Pipping for pointing this out. Cf. #92.

--- ebib.el
+++ ebib.el
@@ -3695,7 +3695,8 @@ If no .bib files are found, return the symbol `none'."
       (if files
           (mapcar (lambda (file)
                     (if (string= file "\\jobname.bib")
-                        (setq file (file-name-nondirectory (file-name-sans-extension (buffer-file-name)))))
+                        (setq file (file-name-nondirectory (concat (file-name-sans-extension (buffer-file-name))
+                                                                   (car ebib-bibtex-extensions)))))
                     ;; If a file has a directory part, we expand it, so
                     ;; `ebib--get-db-from-filename' can match it up with a
                     ;; database's file path.
