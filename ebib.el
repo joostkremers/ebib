4b96690a9c5762ac4df595a3ab90f027530997ef
Joost Kremers <joostkremers@fastmail.fm>
2019-01-25 15:45:59 +0100

    New function: `ebib-import-file'

--- ebib.el
+++ ebib.el
@@ -3715,6 +3715,41 @@ is applied.  If no entry matches, URL is returned unchanged."
         (funcall fn url)
       url)))
 
+(defun ebib-import-file (arg)
+  "Import a file into the database.
+Ask the user for a file path, rename it move it to the first
+directory in `ebib-file-search-dirs'.  The new name is created by
+applying the function in `ebib-name-transform-function' to the
+entry key.  The file extension of the original file is retained.
+If prefix ARG is non-nil, do not delete the original file."
+  (interactive "P")
+  (let* ((key (ebib--get-key-at-point))
+         (file-path (expand-file-name (read-file-name "File to import: " nil nil t)))
+         (ext (file-name-extension file-path))
+         (new-name (ebib--create-file-name-from-key key ext))
+         (dest-dir (file-name-as-directory (car ebib-file-search-dirs)))
+         (dest-path (concat dest-dir new-name))
+         (overwrite nil))
+    (if (not (file-writable-p dest-path))
+        (error "[Ebib] Cannot write file %s" dest-path))
+    (while (and (file-exists-p dest-path)
+                (not overwrite))
+      (let ((choice (read-char-choice (format "File %s already exists; (o)verwrite / (r)ename / (c)ancel? " new-name) '(?o ?r ?c ?q))))
+        (cl-case choice
+          ((?c ?q) (error "[Ebib] Cancelled importing file"))
+          (?r (setq new-name (read-string (format "Change `%s' to: " new-name) new-name))
+              (setq dest-path (concat (file-name-as-directory (car ebib-file-search-dirs)) new-name)))
+          (?o (setq overwrite t)))))
+    (copy-file file-path dest-path)
+    (unless arg
+      (delete-file file-path t))
+    (let ((files (ebib-get-field-value ebib-file-field key ebib--cur-db 'noerror 'unbraced)))
+      (when (or (null files)
+                (not (string-match-p (regexp-quote new-name) files)))
+        (ebib-set-field-value ebib-file-field new-name key ebib--cur-db ebib-filename-separator)
+        (ebib--set-modified t ebib--cur-db)
+        (ebib--update-entry-buffer)))))
+
 ;;; Functions for non-Ebib buffers
 
 (defun ebib-import ()
