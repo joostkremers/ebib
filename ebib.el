3439b7817a5e60d9639df40bfa1e5b987f2320bd
Joost Kremers <joostkremers@fastmail.fm>
2019-10-09 16:19:19 +0200

    Update version number.

--- ebib.el
+++ ebib.el
@@ -6,7 +6,7 @@
 ;; Author: Joost Kremers <joostkremers@fastmail.fm>
 ;; Maintainer: Joost Kremers <joostkremers@fastmail.fm>
 ;; Created: 2003
-;; Version: 2.16
+;; Version: 2.18
 ;; Keywords: text bibtex
 ;; Package-Requires: ((parsebib "2.3") (emacs "25.1"))
 
