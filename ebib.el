139a5c30075974c249942ab20964ac79a4936076
Joost Kremers <joostkremers@fastmail.fm>
2022-04-26 22:34:32 +0200

    Update version number to 2.35
    Also upgrade the minimally required Emacs version to 26.1.

--- ebib.el
+++ ebib.el
@@ -6,10 +6,10 @@
 ;; Author: Joost Kremers <joostkremers@fastmail.fm>
 ;; Maintainer: Joost Kremers <joostkremers@fastmail.fm>
 ;; Created: 2003
-;; Version: 2.34
+;; Version: 2.35
 ;; Keywords: text bibtex
 ;; URL: http://joostkremers.github.io/ebib/
-;; Package-Requires: ((parsebib "2.3") (emacs "25.1"))
+;; Package-Requires: ((parsebib "2.3") (emacs "26.1"))
 
 ;; Redistribution and use in source and binary forms, with or without
 ;; modification, are permitted provided that the following conditions
