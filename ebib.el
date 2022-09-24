242195df13417d273536918ae35c2523a6af8b3b
Hugo Heagren <hugo@heagren.com>
2022-09-24 21:10:02 +0100

    remap `jump-to-register' to `ebib-jump-to-register'

--- ebib.el
+++ ebib.el
@@ -1132,6 +1132,7 @@ keywords before Emacs is killed."
     (define-key map "z" #'ebib-leave-ebib-windows)
     (define-key map "Z" #'ebib-lower)
     (define-key map [remap point-to-register] #'ebib-current-entry-to-register)
+    (define-key map [remap jump-to-register] #'ebib-jump-to-register)
     map)
   "Keymap for the ebib index buffer.")
 
@@ -3849,6 +3850,7 @@ hook `ebib-reading-list-remove-item-hook' is run."
     (define-key map "\C-xk" 'ebib-quit-entry-buffer)
     (define-key map "\C-x\C-s" #'ebib-save-current-database)
     (define-key map [remap point-to-register] #'ebib-current-entry-to-register)
+    (define-key map [remap jump-to-register] #'ebib-jump-to-register)
     map)
   "Keymap for the Ebib entry buffer.")
 
