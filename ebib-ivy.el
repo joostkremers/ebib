16916081291f412df1499d09554baea2c5b01bb9
Joost Kremers <joostkremers@fastmail.fm>
2022-10-09 01:01:32 +0200

    ebib-ivy.el: Use `ebib-edit-current-field` instead of `ebib-edit-field`.
    With the new field editing functionality, the interactive function is called `ebib-edit-curretn-field`.

--- ebib-ivy.el
+++ ebib-ivy.el
@@ -48,7 +48,7 @@
    ("k" ebib-kill-field-contents "kill field contents")
    ("d" ebib-delete-field-contents "delete field contents")
    ("y" ebib-yank-field-contents "yank field contents")
-   ("e" ebib-edit-field "edit field")
+   ("e" ebib-edit-current-field "edit field")
    ("m" ebib--edit-field-as-multiline "edit multiline field")
    ("v" ebib-view-field-as-help "view field as help")
    ("s" ebib-insert-abbreviation "insert abbreviation")
